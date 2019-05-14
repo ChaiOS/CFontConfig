b8d218e1efff1e11610149c91798e914733aea85
Abdurrahman Avci <abdurrahmanavci@gmail.com>
2019-05-14 23:00:04 +0300

    Switch to swift tools version 4.2.

--- Package.swift
+++ Package.swift
@@ -1,18 +1,20 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 
 import PackageDescription
 
 let package = Package(
     name: "CFontConfig",
-    pkgConfig: "fontconfig",
-    providers: [
-        .apt(["libfontconfig1-dev"]),
-        .brew(["fontconfig"])
-    ],
     products: [
         .library(name: "CFontConfig", targets: ["CFontConfig"])
     ],
     targets: [
-        .target(name: "CFontConfig")
+        .systemLibrary(
+            name: "CFontConfig",
+            pkgConfig: "fontconfig",
+            providers: [
+                .apt(["libfontconfig1-dev"]),
+                .brew(["fontconfig"])
+            ]
+        )
     ]
 )
